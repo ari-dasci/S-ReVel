abc032fc27890d1c7fa031a8f0bc5961f9c09629
Iván Sevillano García <isega24@hotmail.com>
2023-01-20 13:50:00 +0100

    ssl cercitifacete

--- ReVel/load_data/__init__.py
+++ ReVel/load_data/__init__.py
@@ -2,8 +2,11 @@ from .CIFAR10 import get_CIFAR10_data
 from .CIFAR100 import get_CIFAR100_data
 from .EMNIST import get_EMNIST_data
 from .FashionMNIST import get_FashionMNIST_data
+import ssl
 
 def load_data(dataset:str="CIFAR10",perturbation = None,train=True,dir="./"):
+    
+    ssl._create_default_https_context = ssl._create_unverified_context
     if dir is None:
         dir = "./"
     if dataset == "CIFAR10":
